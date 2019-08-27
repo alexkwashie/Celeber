12c9a0957b7c24258fa59d984bb8a2f4f7198cc7
Alex A. Kwashie <alex.kwashie@hotmail.com>
2019-08-27 15:00:55 +0100

    Updates

---
+++ precache-manifest.dc849ebc548d465131f953391cfc401d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c57e968d6a0e82f34562b1aa2d853c4",
+    "url": "/celeber/index.html"
+  },
+  {
+    "revision": "0a47b7ea5ce25ea97264",
+    "url": "/celeber/static/css/main.fc2624b5.chunk.css"
+  },
+  {
+    "revision": "de1c0a674dad45b02f3e",
+    "url": "/celeber/static/js/2.aa05ca6d.chunk.js"
+  },
+  {
+    "revision": "0a47b7ea5ce25ea97264",
+    "url": "/celeber/static/js/main.8a454dde.chunk.js"
+  },
+  {
+    "revision": "38222418a0ab194e2a45",
+    "url": "/celeber/static/js/runtime~main.0d751083.js"
+  }
+]);
